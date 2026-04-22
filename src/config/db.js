58cf0172f614d6f36b8d79385a6cefc667f386c3
waqasjafri512 <mr.waqasjafri@gmail.com>
2026-04-22 20:22:33 +0500

    Integrate supabase for production

--- src/config/db.js
+++ src/config/db.js
@@ -2,11 +2,13 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
 });
 
 pool.connect((err) => {
